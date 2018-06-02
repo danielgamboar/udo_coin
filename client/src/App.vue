a8e3230bd6f8ba0175c388b69ce4d8ab6fc76e5b
danielgamboar <danielgrojas94@gmail.com>
2018-06-02 16:24:24 -0400

    Agregando Bootstrapvue y haciendo pruebas en view. Posible login en Index.vue (antiguo Hellow.vue)

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,12 @@
 <template>
-  <div id="app">
     <router-view/>
-  </div>
 </template>
-
 <script>
 export default {
   name: 'App'
 }
 </script>
-
-<style>
+<style scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ client/src/components/test.vue
@@ -0,0 +1,28 @@
+<template>
+    <b-jumbotron header="Bootstrap Vue" lead="Bootstrap 4 Components for Vue.js 2" >
+        <p>For more information visit website</p>
+        <b-btn variant="primary" href="#">More Info</b-btn>
+    </b-jumbotron>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      dismissSecs: 10,
+      dismissCountDown: 0,
+      showDismissibleAlert: false
+    }
+  },
+  methods: {
+    countDownChanged (dismissCountDown) {
+      this.dismissCountDown = dismissCountDown
+    },
+    showAlert () {
+      this.dismissCountDown = this.dismissSecs
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
