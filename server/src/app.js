6e48af0dba5993093f3f056419fc38414eeec257
reppito <repp0731@gmail.com>
2018-06-16 12:27:25 -0400

    adding mongoDB conection

--- server/src/app.js
+++ server/src/app.js
@@ -13,6 +13,13 @@ app.use(cors())
 
 // TEST
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/Users');
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function(callback){
+  console.log("Connection Succeeded");
+});
 
 //Requests
 //TRansaction between 2 address
