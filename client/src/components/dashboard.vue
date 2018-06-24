cfe19f2e24f9766fe2a0a4463371eb1b7eb3f58a
danielgamboar <danielgrojas94@gmail.com>
2018-06-24 18:44:25 -0400

    Rediseno del dashboard del historial de transacciones

--- client/src/components/dashboard.vue
+++ client/src/components/dashboard.vue
@@ -1,26 +1,31 @@
 <template>
 <b-container>
-    <b-card
-        title="Transaction info"
-        subtitle="adress here">
-        <!-- Tambien podriamos colocar un p para la direccion de la wallet que se esta consultando -->
-        <b-row>
-            <b-col class="border">
-                id transaction
-            </b-col>
-            <b-col class="border">
-                concept
-            </b-col>
-        </b-row>
-        <b-row>
-            <b-col class="border">
-                amount
-            </b-col>
-            <b-col class="border">
-                from/to who
-            </b-col>
-        </b-row>
-    </b-card>
+    <b-card-group deck>
+        <b-card header="<b>Transaction List.</b>"
+                class="txtWhite border"
+                header-bg-variant="primary">
+                <!-- aqui se puede agregar el for para las transacciones -->
+            <b-card class="txtBlack border">
+                <p class="grayBackground  txtWhite size"><b>Transaction #1</b></p>
+                <b-row>
+                    <b-col class=" ">
+                        id transaction
+                    </b-col>
+                    <b-col class=" ">
+                        concept
+                    </b-col>
+                </b-row>
+                <b-row>
+                    <b-col class="">
+                        amount
+                    </b-col>
+                    <b-col class="">
+                        from/to who
+                    </b-col>
+                </b-row>
+            </b-card>
+        </b-card>
+    </b-card-group>
 </b-container>
 </template>
 <script>
@@ -28,12 +33,34 @@ export default {
 }
 </script>
 <style scoped>
+.size{
+    width: 50%;
+    margin-left: 25%;
+    margin-right: 25%;
+}
+.grayBackground{
+    background-color: #5c636e;
+}
 .container{
     padding-top: 6em;
     text-align: center;
 }
 .border{
+    border: 1px solid #fff !important;
+}
+.grayBorder{
     border: 1px solid #5c636e !important;
+}
+.txtWhite{
+    color: #fff !important;
+    font-family: 'Ubuntu';
+}
+.txtBlack{
+    color: #5c636e !important;
+    font-family: 'Ubuntu';
+}
+.bg-primary {
+    background-color: #393e46 !important;
 }
 .card{
     position: relative;
